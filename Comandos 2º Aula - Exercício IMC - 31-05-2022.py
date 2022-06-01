ef2accfa0a430467b015bff5e332ff9cec08acc6
Ewerson.Brandina <ewerson.brandina@hotmail.com.br>
2022-06-01 10:01:17 -0300

    Inserção dos números somados como string no label

--- Comandos 2º Aula - Exercício IMC - 31-05-2022.py
+++ Comandos 2º Aula - Exercício IMC - 31-05-2022.py
@@ -1,24 +1,25 @@
 from tkinter import *
-
 #back-end
-def calcular():
-    if in1.get().replace('.','',1).isdigit() and in2.get().replace('.','',1).isdigit():
-        x=round(float(in1.get())/(float(in2.get())**2),2)
-        lb3['text'] = x
-        if x <= 18.5:
-            lb4['text'] = 'Abaixo do Peso'
-        elif x >= 18.6 and x <= 24.9:
-            lb4['text'] = 'Saudável'
-        elif x >= 25 and x <= 29.9:
-            lb4['text'] = 'Peso em Excesso'
-        elif x >= 30 and x <= 34.9:
-            lb4['text'] = 'Obesidade Grau I'
-        elif x >= 35 and x <= 39.9:
-            lb4['text'] = 'Obesidade Grau II'
-        elif x >= 40:
-            lb4['text'] = 'Obesidade Grau III'
-    else:
-        lb3['text'] = 'Erro!!!'
+def zero():
+    in0['text'] = '0'
+def um():
+    in0['text'] = '1'
+def dois():
+    in0['text'] = '2'
+def tres():
+    in0['text'] = '3'
+def quatro():
+    in0['text'] = '4'
+def cinco():
+    in0['text'] = '5'
+def seis():
+    in0['text'] = '6'
+def sete():
+    in0['text'] = '7'
+def oito():
+    in0['text'] = '8'
+def nove():
+    in0['text'] = '9'
 
 #front-end
 root = Tk()
@@ -27,28 +28,81 @@ root.grid_rowconfigure(1, weight=1)
 root.grid_rowconfigure(2, weight=1)
 root.grid_rowconfigure(3, weight=1)
 root.grid_rowconfigure(4, weight=1)
+root.grid_rowconfigure(5, weight=1)
+root.grid_rowconfigure(6, weight=1)
+root.grid_rowconfigure(7, weight=1)
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure(2, weight=1)
-root.bind('q', lambda event: quit())
+root.grid_columnconfigure(3, weight=1)
+#Label and Name
+in0 = Label (root, text='0', font='Arial 25')
+in0.grid(row=1, column=0, columnspan=4, sticky=NSEW)
+lb0 = Label (root, text='CALCULADORA', font='Arial 25', fg='green')
+lb0.grid(row=0, column=0, columnspan=4, sticky=NSEW)
+lb0.config(bg='black')
+#widgets numéricos
+bt0 = Button (root, text='0', font= 'Arial 25')
+bt1 = Button (root, text='1', font= 'Arial 25')
+bt2 = Button (root, text='2', font= 'Arial 25')
+bt3 = Button (root, text='3', font= 'Arial 25')
+bt4 = Button (root, text='4', font= 'Arial 25')
+bt5 = Button (root, text='5', font= 'Arial 25')
+bt6 = Button (root, text='6', font= 'Arial 25')
+bt7 = Button (root, text='7', font= 'Arial 25')
+bt8 = Button (root, text='8', font= 'Arial 25')
+bt9 = Button (root, text='9', font= 'Arial 25')
+#widgets simbolos
+bt10 = Button (root, text=',', font= 'Arial 25')
+bt11 = Button (root, text='=', font= 'Arial 25')
+bt12 = Button (root, text='+', font= 'Arial 25')
+bt13 = Button (root, text='-', font= 'Arial 25')
+bt14 = Button (root, text='x', font= 'Arial 25')
+bt15 = Button (root, text='/', font= 'Arial 25')
+bt16 = Button (root, text='√', font= 'Arial 25')
+bt17 = Button (root, text='x²', font= 'Arial 25')
+bt18 = Button (root, text='%', font= 'Arial 25')
+bt19 = Button (root, text='⌫', font= 'Arial 25')
+bt20 = Button (root, text='C', font= 'Arial 25')
+#Números
+bt0.grid(row=7, column=0, columnspan=2, sticky=NSEW)
+bt1.grid(row=6, column=0, sticky=NSEW)
+bt2.grid(row=6, column=1, sticky=NSEW)
+bt3.grid(row=6, column=2, sticky=NSEW)
+bt4.grid(row=5, column=0, sticky=NSEW)
+bt5.grid(row=5, column=1, sticky=NSEW)
+bt6.grid(row=5, column=2, sticky=NSEW)
+bt7.grid(row=4, column=0, sticky=NSEW)
+bt8.grid(row=4, column=1, sticky=NSEW)
+bt9.grid(row=4, column=2, sticky=NSEW)
+#Simbolos
+bt10.grid(row=7, column=2, sticky=NSEW)
+bt11.grid(row=7, column=3, sticky=NSEW)
+bt12.grid(row=6, column=3, sticky=NSEW)
+bt13.grid(row=5, column=3, sticky=NSEW)
+bt14.grid(row=4, column=3, sticky=NSEW)
+bt15.grid(row=3, column=3, sticky=NSEW)
+bt16.grid(row=3, column=2, sticky=NSEW)
+bt17.grid(row=3, column=1, sticky=NSEW)
+bt18.grid(row=3, column=0, sticky=NSEW)
+bt19.grid(row=2, column=2, columnspan=2, sticky=NSEW)
+bt20.grid(row=2, column=0, columnspan=2, sticky=NSEW)
+#vincular as teclas do teclado
+root.bind('0', lambda event: zero())
+root.bind('1', lambda event: um())
+root.bind('2', lambda event: dois())
+root.bind('3', lambda event: tres())
+root.bind('4', lambda event: quatro())
+root.bind('5', lambda event: cinco())
+root.bind('6', lambda event: seis())
+root.bind('7', lambda event: sete())
+root.bind('8', lambda event: oito())
+root.bind('9', lambda event: nove())
 
-#criar os widgets
-lb1 = Label (root, text='Peso', font= 'Arial 25')
-lb2 = Label (root, text='Altura', font= 'Arial 25')
-in1 = Entry (root, font= 'Arial 25')
-in2 = Entry (root, font= 'Arial 25')
-bt1 = Button (root, text='Calcular', font= 'Arial 25', command=calcular)
-lb3 = Label (root, text=0.0, font= 'Arial 25')
-lb4 = Label (root, text='Interpretação do IMC:', font= 'Arial 25')
 
-#organizar os widgets
-lb1.grid(row=0, column=0, sticky=NSEW)
-lb2.grid(row=1, column=0, sticky=NSEW)
-in1.grid(row=0, column=1, sticky=NSEW)
-in2.grid(row=1, column=1, sticky=NSEW)
-bt1.grid(row=2, column=0, columnspan=3, sticky=NSEW)
-lb3.grid(row=3, column=0, columnspan=3, sticky=NSEW)
-lb4.grid(row=4, column=0, columnspan=3, sticky=NSEW)
+
+
+
 
 #run #Função expecífica para manter a janela aparecendo.
 root.mainloop()
